cae79baf5e7dff5aac833a2c6f115f18aa0b29d9
dyerkkreyd <nandinhovictor78@gmail.com>
2022-12-20 09:16:57 -0300

    Criando modelo

---
+++ src/main/java/projetoif/pi/projeto/models/Projeto.java
@@ -0,0 +1,44 @@
+package projetoif.pi.projeto.models;
+
+public class Projeto {
+
+	private String nome;
+	private String servico;
+	private String data;
+	private String horario;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getServico() {
+		return servico;
+	}
+
+	public void setServico(String servico) {
+		this.servico = servico;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getHorario() {
+		return horario;
+	}
+
+	public void setHorario(String horario) {
+		this.horario = horario;
+	}
+
+	
+
+}
