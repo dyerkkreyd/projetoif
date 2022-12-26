c39c6a9b6c014c1c677dfff7fc331be4ce5ebca2
dyerkkreyd <nandinhovictor78@gmail.com>
2022-12-26 13:35:44 -0300

    Configurando página de detalhes

--- src/main/java/projetoif/pi/projeto/controllers/ProjetosController.java
+++ src/main/java/projetoif/pi/projeto/controllers/ProjetosController.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import net.bytebuddy.dynamic.DynamicType.Builder.FieldDefinition.Optional;
 import projetoif.pi.projeto.models.Projeto;
 import projetoif.pi.projeto.repositories.ProjetoRepository;
 
@@ -28,7 +30,6 @@ public class ProjetosController {
 		
 		System.out.println(projeto);
 		pr.save(projeto);
-		
 		return "projetos/consulta-adicionada";
 	}
 	
@@ -38,8 +39,23 @@ public class ProjetosController {
 		ModelAndView mv = new ModelAndView("lista");
 		mv.addObject("projetos", projetos);
 		return mv; 
+	}
+	
+	@GetMapping("/{id}")
+	public ModelAndView detalhar(@PathVariable Long id) {
+		ModelAndView md = new ModelAndView();
+		java.util.Optional<Projeto> opt = pr.findById(id);
 		
+		if(opt.isEmpty()) {
+			md.setViewName("redirect:/projetos");	
+			return md; 
+		}
 		
+		md.setViewName("eventos/detalhes");
+		Projeto projeto = opt.get();
+		md.addObject("projeto", projeto);
+		
+		return md; 
 	}
 	
 }
