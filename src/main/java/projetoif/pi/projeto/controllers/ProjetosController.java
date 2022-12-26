a48e7f4b979eb6d2e0cedb50e2c070d43fdc629f
dyerkkreyd <nandinhovictor78@gmail.com>
2022-12-26 11:22:13 -0300

    Configurando lista de consultas

--- src/main/java/projetoif/pi/projeto/controllers/ProjetosController.java
+++ src/main/java/projetoif/pi/projeto/controllers/ProjetosController.java
@@ -1,9 +1,13 @@
 package projetoif.pi.projeto.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import projetoif.pi.projeto.models.Projeto;
 import projetoif.pi.projeto.repositories.ProjetoRepository;
@@ -13,7 +17,7 @@ public class ProjetosController {
 	
 	@Autowired
 	private ProjetoRepository pr;
-
+	
 	@RequestMapping("/projeto/form")
 	public String form() {
 		return "formProjeto";
@@ -27,4 +31,15 @@ public class ProjetosController {
 		
 		return "consulta-adicionada";
 	}
+	
+	@GetMapping("/projeto")
+	public ModelAndView listar() {
+		List<Projeto> projetos = pr.findAll();
+		ModelAndView mv = new ModelAndView("lista");
+		mv.addObject("projetos", projetos);
+		return mv; 
+		
+		
+	}
+	
 }
