66a2daaddbe8190a24ccdd36eb990c9137b2da00
dyerkkreyd <nandinhovictor78@gmail.com>
2022-12-28 20:08:53 -0300

    upando projeto final

--- src/main/java/projetoif/pi/projeto/controllers/ProjetosController.java
+++ src/main/java/projetoif/pi/projeto/controllers/ProjetosController.java
@@ -20,117 +20,115 @@ import projetoif.pi.projeto.repositories.ProjetoRepository;
 @RequestMapping("/projetos")
 public class ProjetosController {
 
-@Autowired
-private ProjetoRepository pr;
-@Autowired
-private PacienteRepository pcr;
+	@Autowired
+	private ProjetoRepository pr;
+	@Autowired
+	private PacienteRepository pcr;
 
-@RequestMapping("/form")
-public String form(Projeto projeto) {
-return "projetos/formProjeto";
-}
+	@RequestMapping("/form")
+	public String form(Projeto projeto) {
+		return "projetos/formProjeto";
+	}
 
-@PostMapping
-public String adicionar(Projeto projeto) {
+	@PostMapping
+	public String adicionar(Projeto projeto) {
 
-System.out.println(projeto);
-pr.save(projeto);
+		System.out.println(projeto);
+		pr.save(projeto);
 
-return "redirect:/projetos";
+		return "redirect:/projetos";
 
-}
+	}
 
-@GetMapping
-public ModelAndView listar() {
-List<Projeto> projetos = pr.findAll();
-ModelAndView mv = new ModelAndView("projetos/lista");
-mv.addObject("projetos", projetos);
-return mv;
+	@GetMapping
+	public ModelAndView listar() {
+		List<Projeto> projetos = pr.findAll();
+		ModelAndView mv = new ModelAndView("projetos/lista");
+		mv.addObject("projetos", projetos);
+		return mv;
 
-}
+	}
 
-@GetMapping("/{id}")
-public ModelAndView detalhar(@PathVariable Long id, Paciente paciente) {
-ModelAndView md = new ModelAndView();
-Optional<Projeto> opt = pr.findById(id);
-
-if (opt.isEmpty()) {
-md.setViewName("redirect:/projetos");
-return md;
-}
-md.setViewName("projetos/detalhes");
-Projeto projeto = opt.get();
-md.addObject("projeto", projeto);
-
-return md;
-}
-
-@PostMapping("/{idConsulta}")
-public String salvarPaciente(@PathVariable Long idConsulta, Paciente paciente) {
-
-Optional<Projeto> opt = pr.findById(idConsulta);
-if (opt.isEmpty()) {
-return "redirect:/projetos";
-}
-
-Projeto projeto = opt.get();
-paciente.setProjeto(projeto);
-
-pcr.save(paciente);
-
-return "redirect:/projetos/{idConsulta}";
-
-
-}
-@GetMapping("/{id}/selecionar")
-public ModelAndView selecionarConsulta(@PathVariable Long id){
-ModelAndView md = new ModelAndView();
-Optional<Projeto> opt = pr.findById(id);
-if(opt.isEmpty()) {
-md.setViewName("redirect:/projetos");
-return md;
-}
-Projeto projeto = opt.get();
-md.setViewName("projetos/formProjeto");
-md.addObject("projeto", projeto);
-return md;
-}
-
-@GetMapping("/{id}/remover")
-public String apagarconsulta(@PathVariable Long id) {
-Optional<Projeto> opt = pr.findById(id);
-if (!opt.isEmpty()) {
-Projeto projeto = opt.get();
-pr.delete(projeto);
-}
-return "redirect:/projetos";
-}
-@GetMapping("/{idConsulta}/pacientes/{idPaciente}/selecionar")
-public ModelAndView selecionarPaciente(@PathVariable Long idConsulta, @PathVariable Long idPaciente) {
+	@GetMapping("/{id}")
+	public ModelAndView detalhar(@PathVariable Long id, Paciente paciente) {
 		ModelAndView md = new ModelAndView();
-		
-		Optional<Projeto> optConsulta = pr.findById(idConsulta);
-		Optional<Paciente> optPaciente = pcr.findById(idPaciente);
-		
-		if(optConsulta.isEmpty() || optPaciente.isEmpty() ) {
+		Optional<Projeto> opt = pr.findById(id);
+
+		if (opt.isEmpty()) {
 			md.setViewName("redirect:/projetos");
 			return md;
 		}
-		
-		Projeto projeto = optConsulta.get();
-		Paciente paciente = optPaciente.get();
-		
-		if(projeto.getId() == paciente.getProjeto().getId() ) {
-			md.setViewName("redirect:/projetos");
-			return md;
-			
-		}
-		
 		md.setViewName("projetos/detalhes");
-		md.addObject("paciente", paciente);
+		Projeto projeto = opt.get();
 		md.addObject("projeto", projeto);
-		md.addObject("pacientes",  pcr.findbyProjeto(projeto));
+		md.addObject("pacientes", pcr.findAllByProjeto(projeto));
+		System.out.println(pcr.findAllByProjeto(projeto));
 		
 		return md;
+	}
+
+	@PostMapping("/{idConsulta}")
+	public String salvarPaciente(@PathVariable Long idConsulta, Paciente paciente) {
+
+		Optional<Projeto> opt = pr.findById(idConsulta);
+		if (opt.isEmpty()) {
+			return "redirect:/projetos";
+		}
+
+		Projeto projeto = opt.get();
+		paciente.setProjeto(projeto);
+
+		pcr.save(paciente);
+
+		return "redirect:/projetos/{idConsulta}";
+
+	}
+
+	@GetMapping("/{id}/selecionar")
+	public ModelAndView selecionarConsulta(@PathVariable Long id) {
+		ModelAndView md = new ModelAndView();
+		Optional<Projeto> opt = pr.findById(id);
+		if (opt.isEmpty()) {
+			md.setViewName("redirect:/projetos");
+			return md;
+		}
+		Projeto projeto = opt.get();
+		md.setViewName("projetos/formProjeto");
+		md.addObject("projeto", projeto);
+		
+		return md;
+	}
+
+	@GetMapping("/{id}/remover")
+	public String apagarconsulta(@PathVariable Long id) {
+		Optional<Projeto> opt = pr.findById(id);
+		if (!opt.isEmpty()) {
+			Projeto projeto = opt.get();
+			pr.delete(projeto);
+		}
+		return "redirect:/projetos";
+	}
+
+	@GetMapping("/{idConsulta}/pacientes/{idPaciente}/selecionar")
+	public ModelAndView selecionarPaciente(@PathVariable Long idConsulta, @PathVariable Long idPaciente) {
+		ModelAndView md = new ModelAndView();
+
+		Optional<Projeto> optConsulta = pr.findById(idConsulta);
+		Optional<Paciente> optPaciente = pcr.findById(idPaciente);
+
+		if (optConsulta.isEmpty() || optPaciente.isEmpty()) {
+			md.setViewName("redirect:/projetos");
+			return md;
+		}
+
+		Projeto projeto = optConsulta.get();
+		Paciente paciente = optPaciente.get();
+
+		if (projeto.getId() == paciente.getProjeto().getId()) {
+			md.setViewName("redirect:/projetos/{idConsulta}");
+			return md;
+		}
+		return md;
+	}
 }
-}
+
--- src/main/java/projetoif/pi/projeto/repositories/PacienteRepository.java
+++ src/main/java/projetoif/pi/projeto/repositories/PacienteRepository.java
@@ -1,9 +1,14 @@
 package projetoif.pi.projeto.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import projetoif.pi.projeto.models.Paciente;
+import projetoif.pi.projeto.models.Projeto;
 
-public interface PacienteRepository extends JpaRepository<Paciente, Long>{
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+
+	List<Paciente> findAllByProjeto(Projeto projeto);
 
 }
